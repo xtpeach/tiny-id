e1e9e2f374b7460876fd4e693e8923281dadf55d
xtpeach <xtpeach@outlook.com>
2023-02-12 13:00:40 +0800

    调整tiny_id_info表初始化数据逻辑，不修改之前已经初始化的数据记录

--- tiny-id-server/src/main/java/com/xtpeach/tinyid/server/init/executor/TinyIdInfoExecutor.java
+++ tiny-id-server/src/main/java/com/xtpeach/tinyid/server/init/executor/TinyIdInfoExecutor.java
@@ -1,5 +1,6 @@
 package com.xtpeach.tinyid.server.init.executor;
 
+import com.alibaba.nacos.common.utils.StringUtils;
 import com.xtpeach.tiny.id.xls.executor.XLSExecutor;
 import com.xtpeach.tinyid.server.dao.entity.TinyIdInfo;
 import com.xtpeach.tinyid.server.init.repo.TinyIdInfoRepo;
@@ -21,6 +22,18 @@ public class TinyIdInfoExecutor extends XLSExecutor<TinyIdInfo> implements Comma
     public void run(String... args) {
         List<TinyIdInfo> tinyIdInfoList = ImportDataFromXLS(TinyIdInfo.class, "tinyid");
         tinyIdInfoList.removeIf(tinyIdInfo -> ObjectUtils.isEmpty(tinyIdInfo.getId()));
+        // 获取已有的 tiny_id_info 如果已经有了就不进行初始化
+        List<TinyIdInfo> tinyIdInfoListSaved = repo.findAll();
+        if (CollectionUtils.isNotEmpty(tinyIdInfoListSaved)) {
+            tinyIdInfoListSaved.removeIf(
+                    tinyIdInfoSaved ->
+                            !ObjectUtils.isEmpty(
+                                    tinyIdInfoList.stream().filter(tinyIdInfo
+                                            -> tinyIdInfoSaved.getId().longValue() == tinyIdInfo.getId().longValue())
+                                            .findFirst().orElse(null)
+                            )
+            );
+        }
         if (CollectionUtils.isNotEmpty(tinyIdInfoList)) {
             repo.saveAll(tinyIdInfoList);
         }
